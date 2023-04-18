d873129aad32768e9c2832d4d532aa6511b86f54
esamohammad <esa.ce2020@gmail.com>
2023-04-18 23:13:31 +0600

    server is initially ok with params id

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const port = 3000;
+
+// json file import
+const courses = require("./courses.json");
+
+
+app.get("/", (req, res) => {
+  res.send("server is ready")
+});
+
+// data api get....(all Data api)
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+// data api get....(Single id --Data api)
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const single_course = courses.find(course => course._id === id) ||{};
+  res.send(single_course)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
