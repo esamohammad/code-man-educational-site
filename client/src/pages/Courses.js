b6dc4d6d2323f8111ca51f9ecf7df7c7b627eb8b
esamohammad <esa.ce2020@gmail.com>
2023-04-23 17:10:58 +0600

    course style

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import SingleCourse from "../components/SingleCourse";
-
+import "../style/courses.css";
 
 const Courses = () => {
   const courses = useLoaderData();
@@ -10,41 +10,33 @@ const Courses = () => {
   return (
     <div>
       {/* courses page ---1. grid md:grid-4 part kora*/}
-      <div className="md:grid grid-cols-4 mb-5">
-       
-       
+      <div className="mb-5 grid-cols-4 md:grid">
         {/* sidebar---2. */}
-        <aside class="  relative md:top-0 md:sticky md:h-screen">
+        <aside class="  relative md:sticky md:top-0 md:h-screen">
           {/* // Fixed Sidebar */}
-          <div className="bg-yellow-500 rounded-2xl h-full  md:ml-4 mx-4  p-4 shadow-layer ">
+          <div className=" mx-4 h-full  rounded-2xl bg-yellow-500  p-4 md:ml-4 ">
             {courses.map((course) => (
               <Sidebar course={course} key={course._id}></Sidebar>
             ))}
           </div>
         </aside>
 
-
         {/* course window */}
-        <div className="col-span-3 px-8 md:px-12 md:mb-2.25 ">
+        <div className="md:mb-2.25 col-span-3 px-8 md:px-12 ">
           {
-            <div className="md:grid grid-cols-3 gap-10 ">
+            <div className="grid-cols-3 gap-10 md:grid ">
               {courses.map((course) => (
                 <SingleCourse course={course} key={course._id}></SingleCourse>
               ))}
             </div>
           }
         </div>
-        
       </div>
     </div>
   );
 };
 
 export default Courses;
-
-
-
-
 
 /*
 
