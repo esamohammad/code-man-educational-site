3e90d4fe641435986f98fc886d94268905d889b9
esamohammad <esa.ce2020@gmail.com>
2023-04-23 23:19:06 +0600

    61-8 google singin logout done

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,122 +4,129 @@ import { AuthContext } from "../context/AuthProvider";
 import { FaUser } from 'react-icons/fa';
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
 
-  return (
-    <div className="mb-  navbar">
-      <div className="navbar-start ">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn-ghost btn lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-          >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li tabIndex={0}>
-              <Link to="/courses">Courses</Link>
-            </li>
-            <li>
-              <Link to="/faq">FAQ</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/login" className="btn mb-2 text-white">
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link to="/register" className="btn  text-white">
-                Register
-              </Link>
-            </li>
-          </ul>
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
+
+    return (
+        <div className="mb-  navbar">
+            <div className="navbar-start ">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn-ghost btn lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+                    >
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li tabIndex={0}>
+                            <Link to="/courses">Courses</Link>
+                        </li>
+                        <li>
+                            <Link to="/faq">FAQ</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog">Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/login" className="btn mb-2 text-white">
+                                Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/register" className="btn  text-white">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <Link
+                    to="./"
+                    className="ms-0 text-lg font-black normal-case  sm:ms-4 sm:text-2xl "
+                >
+                    CodeMan
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex ">
+                <ul className="menu menu-horizontal px-1">
+                    <li className="font-bold md:text-xl ">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="font-bold md:text-xl ">
+                        <Link to="/courses">Courses</Link>
+                    </li>
+                    <li className="font-bold md:text-xl ">
+                        <Link to="/faq">FAQ</Link>
+                    </li>
+                    <li className="font-bold md:text-xl ">
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="navbar-end">
+                <Link to="" className="md:mr-15 md:me-2">
+                    {user?.photoURL ?
+
+
+                        // ! avatar
+                        <div className="avatar online mt-2">
+                            <div className="w-8 lg:w-12 rounded-full ring ring-white ring-offset-yellow-500 ring-offset-4">
+                                <img src={user?.photoURL} alt="" />
+                            </div>
+                        </div>
+                        // ! avatar enb
+                        : <span className="text-xl lg:text-2xl"><FaUser></FaUser></span>
+                    }
+                </Link>
+
+
+                <Link to="" className="font-bold md:text-xl md:mr-15 md:me-4 ">
+                    {
+                        user?.uid ?
+                            <>
+                                <span className="lg:me-2">{user?.displayName}</span>
+                                <button className="btn " onClick={handleLogOut}>Log out</button>
+                            </>
+                            :
+                            <>
+                                <span className="btn">
+                                    <Link to='/login'>Login</Link>
+                                </span>
+
+                            </>
+                    }
+                </Link>
+
+
+
+                {/* <Link to="/login" className="btn me-2  md:mr-10">
+                    Login
+                </Link> */}
+            </div>
         </div>
-        <Link
-          to="./"
-          className="ms-0 text-lg font-black normal-case  sm:ms-4 sm:text-2xl "
-        >
-          CodeMan
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex ">
-        <ul className="menu menu-horizontal px-1">
-          <li className="font-bold md:text-xl ">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="font-bold md:text-xl ">
-            <Link to="/courses">Courses</Link>
-          </li>
-          <li className="font-bold md:text-xl ">
-            <Link to="/faq">FAQ</Link>
-          </li>
-          <li className="font-bold md:text-xl ">
-            <Link to="/blog">Blog</Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="navbar-end">
-     
-        <Link to="" className="font-bold md:text-xl md:mr-15 md:me-4 ">
-          {user?.displayName}
-        </Link>
-
-        <Link to="" className="md:mr-15 md:me-2">
-      {user?.photoURL ?
-//avatar
-
-
-//  <img src={user.photoURL} style={{ height: '50px'}}  alt="pic" /> 
-
-<div className="avatar online mt-2">
-  <div className="w-8 lg:w-12 rounded-full ring ring-white ring-offset-yellow-500 ring-offset-4">
-    <img src={user.photoURL} alt=""/>
-  </div>
-</div>
-
-
-
- 
- 
- 
- 
- 
- 
- 
- 
- : <FaUser></FaUser>
- }
-        </Link>
-
-
-
-
-        <Link to="/login" className="btn me-2  md:mr-10">
-         Login
-        </Link>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -1,10 +1,10 @@
-import React, { createContext,useEffect, useState } from "react";
-import {getAuth,signInWithPopup,onAuthStateChanged} from 'firebase/auth'
+import React, { createContext, useEffect, useState } from "react";
+import { getAuth, signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth'
 import app from '../firbase/firebase.config';
 
 
 export const AuthContext = createContext();
-const auth =getAuth(app);
+const auth = getAuth(app);
 
 
 
@@ -13,23 +13,32 @@ const AuthProvider = ({ children }) => {
 
   const providerLogin = (provider) => {
     return signInWithPopup(auth, provider);
-}
-
-//observer set for manage user state
-useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log('inside auth state change', currentUser);
-      setUser(currentUser)
-  });
-
-  return () => {
-      unsubscribe();
   }
 
-}, [])
 
-  const authInfo = { user,
-  providerLogin };
+  //! logout authentication
+  const logOut = () => {
+    return signOut(auth);
+  }
+
+  //observer set for manage user state
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log('inside auth state change', currentUser);
+      setUser(currentUser)
+    });
+
+    return () => {
+      unsubscribe();
+    }
+
+  }, [])
+
+  const authInfo = {
+    user,
+    providerLogin,
+    logOut
+  };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
